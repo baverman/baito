2bcf86a99be229647f9d1bdc278502bbead3ecfa
Anton Bobrov <bobrov@vl.ru>
2011-11-19 23:18:21 +1100

    werkzeug -> webob

--- baito/app.py
+++ baito/app.py
@@ -1,34 +1,38 @@
-from werkzeug.wrappers import Request, Response
-from werkzeug.wsgi import ClosingIterator
-from werkzeug.exceptions import HTTPException
-from werkzeug.routing import Map, Rule
-from werkzeug.serving import run_simple
+from routes.mapper import Mapper
+from webob import Request, Response
+from webob.exc import HTTPException, HTTPNotFound
+from wsgiref.simple_server import make_server
 
 class App(object):
     def __init__(self):
-        self.url_map = Map()
+        self.url_map = Mapper()
         self.Request = Request
         self.Response = Response
 
     def expose(self, rule, **kwargs):
         def decorator(func):
             kwargs['endpoint'] = func
-            self.url_map.add(Rule(rule, **kwargs))
+            self.url_map.connect(None, rule, **kwargs)
             return func
 
         return decorator
 
     def __call__(self, environ, start_response):
         request = self.Request(environ)
-        adapter = self.url_map.bind_to_environ(environ)
         try:
-            endpoint, values = adapter.match()
-            response = endpoint(request, **values)
+            result = self.url_map.match(None, environ)
+            if not result:
+                raise HTTPNotFound()
+
+            endpoint = result.pop('endpoint')
+            response = endpoint(request, **result)
             if isinstance(response, basestring):
                 response = self.Response(response)
         except HTTPException, e:
             response = e
-        return ClosingIterator(response(environ, start_response), [])
 
-    def run(self, host='localhost', port=8000, use_reloader=True, **kwargs):
-        run_simple(host, port, self, use_reloader=use_reloader, **kwargs)
+        return response(environ, start_response)
+
+    def run(self, host='localhost', port=8000):
+        httpd = make_server(host, port, self)
+        httpd.serve_forever()
