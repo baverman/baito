15a8437d5013ce01fbd93871dc85f50dee2dbe05
Anton Bobrov <bobrov@vl.ru>
2011-11-21 22:47:36 +1100

    pass additional arguments to jinja Environment

--- baito/render/jinja.py
+++ baito/render/jinja.py
@@ -1,11 +1,17 @@
 from jinja2 import Environment, PackageLoader, StrictUndefined
 
 class Renderer(object):
-    def __init__(self, package, folder='templates'):
+    def __init__(self, package, folder='templates', **kwargs):
+        kwargs.update({
+            'undefined': StrictUndefined,
+            'extensions': ['jinja2.ext.with_'],
+            'line_statement_prefix': '#',
+            'line_comment_prefix': '##',
+        })
+
         self.env = Environment(
             loader=PackageLoader(package, 'templates'),
-            undefined = StrictUndefined,
-            extensions = ['jinja2.ext.with_']
+            **kwargs
         )
 
     def __call__(self, Response, name, result):
